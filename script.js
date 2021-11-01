77634d6163ea36e4dd39c07663d0d24cce06436b
laineysu <63746471+laineysu@users.noreply.github.com>
2021-11-01 22:10:26 +0000

    first commit of lab submission

---
+++ script.js
@@ -0,0 +1,8 @@
+//Week 7 Lab 01Nov21 Helene Suttle javascript	
+function myInputs(email,userName,myDiv){	
+	var email = document.getElementById("email").value;
+	var userName = document.getElementById("userName").value;
+	document.getElementById("myForm").style.display="none";
+	alert(userName+" thank you for your details. We will be in touch via "+email+" shortly.");	
+}
+
